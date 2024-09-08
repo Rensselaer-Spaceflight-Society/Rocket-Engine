4f49d39f0f6918cd6e7d8b040aa9992c1150df06
Cooper Werner <cooperwerner824@gmail.com>
2024-09-08 19:24:29 -0400

    implementing feedback

--- src/control_room/main.cpp
+++ src/control_room/main.cpp
@@ -1,8 +1,9 @@
+#include <iostream>
+
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
-#include <iostream>
 
 #include "ui.hpp"
 
