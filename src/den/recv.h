b1123cc3a81f85d5f5f7382f8d24484e0349fcb7
Dennis Kwarteng <luispekerson@gmail.com>
2024-10-13 18:34:27 -0400

    Included a basic command queue check

--- src/den/recv.h
+++ src/den/recv.h
@@ -3,15 +3,17 @@
 
 #include <QSerialPort>
 #include <QThread>
+#include <QList>
 #include <QMutex>
 #include <QDebug>
+#include "command.h"
 
 class recv : public QThread
 {
     Q_OBJECT
 
 public:
-    explicit recv(QSerialPort* port,bool* retrans, QObject* parent = nullptr) : QThread(parent), serial(port), retransmit_bool(retrans) {}
+    explicit recv(QSerialPort* port, bool* retrans, QList<command> *commands, QObject* parent = nullptr) : QThread(parent), serial(port), command_queue(commands), retransmit_bool(retrans) {}
 
 public slots:
     void startReceiving() {
@@ -27,11 +29,20 @@ protected:
 
         if (serial->isOpen()) {
             if (serial->waitForReadyRead(1000)) {
-                QByteArray data = serial->readAll();
-                qDebug() << "Data received:" << data;
+                auto i = command_queue->begin();
+                QByteArray data = serial->read(8);
+
+                // Check to see if it's an acknowledgement or data
+                if(data == i->getCommand().toUtf8()){
+                    command_queue->erase(i);                                // If it's an ack for the expected command, remove it from the list
+                }else{
+                    data += serial->read(33);                                // Otherwise its probably data, keep reading the rest of the data
+                }
+
+                // qDebug() << "Data received:" << data;
                 *retransmit_bool = false;
             }else {
-                qDebug() << "Retransmission number: " << retransmit++;
+                // qDebug() << "Retransmission number: " << retransmit++;
                 if(retransmit == 5){
                     qDebug() << "Retranmission attemps exceeded";
                     return;
@@ -41,13 +52,14 @@ protected:
 
             emit receiveFinished();
         } else {
-            qDebug() << "Error opening serial port for receiving.";
+            // qDebug() << "Error opening serial port for receiving.";
         }
         qDebug() << "Receive Thread done";
     }
 
 private:
     QSerialPort *serial;
+    QList<command> *command_queue;
     int retransmit = 0;
     bool *retransmit_bool;
 };
